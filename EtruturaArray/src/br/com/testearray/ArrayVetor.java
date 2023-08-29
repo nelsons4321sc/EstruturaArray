85db4b73b2dd436135f1e0f6af8ce723858c4784
Nelson Costa <nelsonscosta@yahoo.com.br>
2023-08-29 17:09:56 -0300

    iimpressão dinâmica

--- EtruturaArray/src/br/com/testearray/ArrayVetor.java
+++ EtruturaArray/src/br/com/testearray/ArrayVetor.java
@@ -12,12 +12,18 @@ public class ArrayVetor {
 		notas[1] = 4.8;
 		notas[2] = 6.8;
 		notas[3] = 7.8;
-		
+		//retirando o manual
+		/*
 		System.out.println("Nota 1 = "+notas[0]);
 		System.out.println("Nota 2 = "+notas[1]);
 		System.out.println("Nota 3 = "+notas[2]);
 		System.out.println("Nota 4 = "+notas[3]);
-		
+		*/
+		//adicionado dinamicamente
+		for(int pos=0; pos<=3; pos++) {
+			//tem que ser adicionado a pos +1 entre parenteses, para imprimir as notas corretamente
+			System.out.println("Nota "+(pos+1)+" = "+notas[pos]);
+		}
 		
 	}
 
