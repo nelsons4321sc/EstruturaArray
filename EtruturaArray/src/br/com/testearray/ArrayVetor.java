8de3b8cdc5bc152486bd23536245f9cf0322ccba
Nelson Costa <nelsonscosta@yahoo.com.br>
2023-08-29 16:40:38 -0300

    Estrura de array de notas

---
+++ EtruturaArray/src/br/com/testearray/ArrayVetor.java
@@ -0,0 +1,19 @@
+package br.com.testearray;
+
+public class ArrayVetor {
+
+	public static void main(String[] args) {
+		
+		//Array deve ter uma quantidade de posições definidas, e pode ser todos ostipos de dados e objetos
+		double[] notas = new double[4];
+		
+		notas[0] = 8.8;
+		notas[1] = 4.8;
+		notas[2] = 6.8;
+		notas[3] = 7.8;
+		
+		System.out.println(notas[1]);
+		
+	}
+
+}
